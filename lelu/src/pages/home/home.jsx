568fa80e9d512fe6bf2d97c75785cf631edf9401
DanielRGabriel <danielrgabriellelu@gmail.com>
2024-10-04 15:40:37 -0300

    inclusao de produtos total 15

--- lelu/src/pages/home/home.jsx
+++ lelu/src/pages/home/home.jsx
@@ -7,31 +7,39 @@ import Logo from '../../components/Logo/logo.jsx';
 import Footer from '../../components/Footer/footer.jsx';
 import styles from './home.module.css';
 
-import ecobagImage from '../../assets/ImagensProdutos/ecobag3.png';
-import carteiraImage from '../../assets/ImagensProdutos/carteira_solange1.png';
+import ecobag3Image from '../../assets/ImagensProdutos/ecobag3.png';
+import carteiraSolange1Image from '../../assets/ImagensProdutos/carteira_solange1.png';
 import necessaireImage from '../../assets/ImagensProdutos/necessaire1.png';
-import bolsaMochilaImage from '../../assets/ImagensProdutos/bolsa_mochila2.png';
-import bolsaTransversalImage from '../../assets/ImagensProdutos/bolsa_tranversal3.png';
+import bolsaMochila2Image from '../../assets/ImagensProdutos/bolsa_mochila2.png';
+import bolsaTransversal3Image from '../../assets/ImagensProdutos/bolsa_tranversal3.png';
 import pochetelImage from '../../assets/ImagensProdutos/pochete1.png';
 import absorventesImage from '../../assets/ImagensProdutos/absorventes1.png';
+import vestidoBebe1Image from '../../assets/ImagensProdutos/vestido_bebe1.png';
+import portaOculos1Image from '../../assets/ImagensProdutos/porta_oculos1.png';
+import portaDocumentos1Image from '../../assets/ImagensProdutos/porta_documentos1.png';
+import capaCaderno2Image from '../../assets/ImagensProdutos/capa_caderno2.png';
+import pantufas1Image from '../../assets/ImagensProdutos/pantufas1.png';
+import necessaireDog1Image from '../../assets/ImagensProdutos/necessaire_dog1.png';
+import ecobag1Image from '../../assets/ImagensProdutos/ecobag1.png';
+import vestidoBebe2Image from '../../assets/ImagensProdutos/vestido_bebe2.png';
 
 // Adicionando categorias aos produtos
 const cardData = [
-  { title: 'Ecobag', content: 'Ecobag feita à mão', price: 49.90, image: ecobagImage, category: 'Bolsas' },
-  { title: 'Carteira Solange', content: 'Carteira de tecido', price: 34.90, image: carteiraImage, category: 'Acessórios' },
+  { title: 'Ecobag', content: 'Ecobag feita à mão', price: 49.90, image: ecobag3Image, category: 'Bolsas' },
+  { title: 'Carteira Solange', content: 'Carteira de tecido', price: 34.90, image: carteiraSolange1Image, category: 'Acessórios' },
   { title: 'Necessaire grande', content: 'Necessaire espaçosa', price: 49.90, image: necessaireImage, category: 'Acessórios' },
-  { title: 'Bolsa Mochila', content: 'Bolsa para caminhar', price: 49.90, image: bolsaMochilaImage, category: 'Bolsas' },
-  { title: 'Bolsa Transversal', content: 'Mochila espaçosa para uso diário', price: 69.90, image: bolsaTransversalImage, category: 'Bolsas' },
+  { title: 'Bolsa Mochila', content: 'Bolsa para caminhar', price: 49.90, image: bolsaMochila2Image, category: 'Bolsas' },
+  { title: 'Bolsa Transversal', content: 'Mochila espaçosa para uso diário', price: 69.90, image: bolsaTransversal3Image, category: 'Bolsas' },
   { title: 'Pochetes', content: 'Pochetes estampadas', price: 49.90, image: pochetelImage, category: 'Acessórios' },
   { title: 'Absorventes de pano', content: 'Quatro tamanhos disponíveis', price: 22.90, image:absorventesImage, category: 'Diversos' },
-  { title: 'Chaveiro Personalizado', content: 'Chaveiro de couro com iniciais', price: 19.90, image: 'image_path_here', category: 'Acessórios' },
-  { title: 'Pochete Unissex', content: 'Pochete prática e estilosa', price: 49.90, image: 'image_path_here', category: 'Acessórios' },
-  { title: 'Bolsa Tote', content: 'Bolsa tote grande de tecido', price: 89.90, image: 'image_path_here', category: 'Bolsas' },
-  { title: 'Mochila de Couro', content: 'Mochila elegante de couro', price: 199.90, image: 'image_path_here', category: 'Mochilas' },
-  { title: 'Porta-Cartão', content: 'Porta-cartão compacto em couro', price: 39.90, image: 'image_path_here', category: 'Acessórios' },
-  { title: 'Necessaire Compacta', content: 'Necessaire ideal para maquiagem', price: 39.90, image: 'image_path_here', category: 'Acessórios' },
-  { title: 'Bolsa de Viagem', content: 'Bolsa de viagem espaçosa e resistente', price: 249.90, image: 'image_path_here', category: 'Bolsas' },
-  { title: 'Pulseira de Couro', content: 'Pulseira masculina de couro trançado', price: 69.90, image: 'image_path_here', category: 'Acessórios' },
+  { title: 'Vestido de bebê melancia', content: '3 meses a 2 anos', price: 49.90, image:vestidoBebe1Image, category: 'Roupas' },
+  { title: 'Porta Óculos', content: 'Cabem 2 óculos', price: 24.90, image: portaOculos1Image, category: 'Acessórios' },
+  { title: 'Porta Documentos', content: 'Pequena carteira', price: 14.90, image:portaDocumentos1Image, category: 'Acessórios' },
+  { title: 'Capa de caderno', content: 'Proteja seu caderno / livro', price: 39.90, image: capaCaderno2Image, category: 'Acessórios' },
+  { title: 'Pantufa', content: 'Super Confortáveis', price: 64.90, image: pantufas1Image, category: 'Acessórios' },
+  { title: 'Necessaire Compacta', content: 'Necessaire ideal para maquiagem', price: 39.90, image: necessaireDog1Image, category: 'Acessórios' },
+  { title: 'Ecobag', content: 'Ecobag feita à mão', price: 49.90, image: ecobag1Image, category: 'Bolsas' },
+  { title: 'Vestido de bebê ursinhos', content: '3 meses a 2 anos', price: 49.90, image:vestidoBebe2Image, category: 'Roupas' },
   
 ];
 
